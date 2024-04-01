78ec99fe1abe4e695446b864cc24b90fdce76a26
s3e3 <48084258+siatos@users.noreply.github.com>
2024-04-01 13:09:34 +0300

    updates

--- ge1.py
+++ ge1.py
@@ -1,4 +1,4 @@
-def name_and_values(pos):
+def protein_name_and_values(pos):
     """ Get name and values list for a protein
         Return: a list of protein and values
     """
@@ -23,11 +23,11 @@ def name_and_values(pos):
 
 
 if __name__ == '__main__':
-    L = name_and_values("First")
+    L = protein_name_and_values("First")
     print("First List is {}".format(L))
     L1 = L[1]
     print("Values for fist are {}".format(L1))
-    L = name_and_values("Second")
+    L = protein_name_and_values("Second")
     print("Second List is {}".format(L))
     L2 = L[1]
     print("Values for second are {}".format(L2))
@@ -37,20 +37,20 @@ if __name__ == '__main__':
     if any(item == 10 for item in L3):
         print("Found 10 in at least one pos")
     else:
-        print("Nothing found")
+        print("Nothing found - Value 10 is not in the list")
     # another way for the above    
     for ind, item in enumerate(L3):
         if item == 10:
            print("Found 10 at pos {}".format(ind))
-    L = name_and_values("Third")
+    L = protein_name_and_values("Third")
     L3 = L3 + L[1]
-    print("The new list - all values of the lists joined: {}".format(L3))
+    print("New list - all values of the lists joined: {}".format(L3))
     L3 = L3[2:]
-    print("The new list of values without the first list is: {}".format(L3))
+    print("New list of values without the first list is: {}".format(L3))
     # one way to reverse the list using slice end to start with step -1
-    # print("The new list without the first list reversed is: {}".format(L3[::-1]))
+    # print("New list without the first list reversed is: {}".format(L3[::-1]))
     # or we can reverse the list in place using list.reverse()
     L3.reverse()
-    print("The new list of values without the first list reversed is: {}".format(L3))
+    print("new list of values without the first list reversed is: {}".format(L3))
 
 
